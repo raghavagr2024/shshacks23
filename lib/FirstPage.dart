57729b8ddf3fa20020cac2d25f78d47649c7a5b2
Raghav Agrawal <raghavagr2024@gmail.com>
2023-03-04 16:32:03 -0600

    got sign up page done

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:shshacks23/SignupPage.dart';
 
-
-class FirstPage extends StatelessWidget{
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 100,),
-          LoginButtonSender(),
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 100,
+          ),
+          Center(
+            child: LoginButtonSender(),
+          ),
+          Center(
+            child: SizedBox(
+              height: 50,
+            ),
+          ),
           SignupButtonSender()
         ],
       ),
     );
   }
-
 }
 
-class LoginButtonSender extends StatelessWidget{
+class LoginButtonSender extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {  },
+      onPressed: () {},
       child: Text("Login"),
     );
   }
 }
 
-class SignupButtonSender extends StatelessWidget{
+class SignupButtonSender extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {  },
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignupPage()),
+        );
+      },
       child: Text("Sign Up"),
     );
   }
